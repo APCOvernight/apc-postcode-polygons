65dd8d356f168ef7d6dd4e102cc17a986e1ab19a
Mike Hingley <mike.hingley@apc-overnight.com>
2018-04-04 13:59:12 +0100

    fixing reference

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 'use strict'
-exports = module.exports = require('PostcodeSectors.json')
+//exports = module.exports = require('./PostcodeSectors.json')
+var polly_gons = require('./PostcodeSectors')
+function returnPolly(){
+return polly_gons
+}
+
+
+module.exports = returnPolly
